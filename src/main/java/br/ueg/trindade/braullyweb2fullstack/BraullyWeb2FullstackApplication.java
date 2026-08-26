c851a83f932d3bf8519b4343d1eb89cbc37cf554
danillo.sdd <danillo.sdd@hotmail.com>
2026-08-26 20:56:22 -0300

    feat: Inicialização do projeto Spring Boot e React

---
+++ src/main/java/br/ueg/trindade/braullyweb2fullstack/BraullyWeb2FullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.braullyweb2fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraullyWeb2FullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraullyWeb2FullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/trindade/braullyweb2fullstack/BraullyWeb2FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.braullyweb2fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraullyWeb2FullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
